5cd1a0d1e36b5f9f9ab0f7de30bdaeb7f7f31ca9
Mihir Sahu <2002mihir@gmail.com>
2023-12-27 02:23:08 -0600

    Set up Supabase

--- README.md
+++ README.md
@@ -10,3 +10,7 @@
 - [NextJS Server Actions](https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions-and-mutations)
 - [Auth0 Server Actions Blog](https://auth0.com/blog/using-nextjs-server-actions-to-call-external-apis/)
 - [LogRocket Server Actions Blog](https://blog.logrocket.com/diving-into-server-actions-next-js-14/)
+- [How to use Next.js 14 Server Actions](https://www.youtube.com/watch?v=5MRLO-7O2So)
+### Supabase
+- [Supabase JavaScript Client Library Docs](https://supabase.com/docs/reference/javascript/installing)
+- [Supabase Authentication Docs](https://supabase.com/docs/guides/auth/auth-helpers/nextjs?language=ts)
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 const Navbar = () => {
 
   return(
     <div className="flex flex-row w-full justify-between p-10 text-black">
-      <div className="font-bold">DALLE UI</div>
+      <Link href='\' className="font-bold">DALLE UI</Link>
       <div className="flex flex-row space-x-10">
-        <button className="text-darkGray hover:text-black">API Key</button>
-        <button className="text-darkGray hover:text-black">History</button>
+        <button className="text-darkGray hover:text-black">About</button>
+        <Link href="\login" className="text-darkGray hover:text-black">Log in</Link>
       </div>
     </div>
   );
