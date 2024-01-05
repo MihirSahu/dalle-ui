b1c9115be00d04fdebd05f6b6dde9864d80f01e2
Mihir Sahu <2002mihir@gmail.com>
2024-01-04 20:56:23 -0600

    Add footer

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Container, Group, Anchor } from '@mantine/core';
+import Image from 'next/image';
+
+export default function Footer() {
+
+  return (
+    <div className='flex flex-row bottom-0 pb-5 w-max sm:absolute'>
+      <Container>
+        <span style={{ display: 'inline-flex', alignItems: 'center' }}>
+          Made with
+          <Image style={{marginLeft: '8px', marginRight: '8px'}} src="/heart.png" alt='❤️' width={20} height={20} />
+          by&nbsp; <a href='https://mihirsahu.com' target='_blank' style={{color: 'red'}}>Mihir Sahu</a>
+        </span>
+      </Container>
+    </div>
+  );
+}
