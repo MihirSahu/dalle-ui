69543f47c715fc0c9ac6aa27c8620e3b4b8f3384
Mihir Sahu <2002mihir@gmail.com>
2024-01-12 23:41:08 -0600

    feat: Add image download option

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { notifications } from '@mantine/notifications';
+import Image from 'next/image';
 
 const History = () => {
   const [URLs, setURLs] = useState([]);
@@ -10,9 +11,9 @@ const History = () => {
     const fetchHistory = async () => {
       const formData = new URLSearchParams();
       formData.append('offset', '0');
-      formData.append('numImages', '5');
+      formData.append('numImages', '10');
 
-      const response = await fetch('/components/history', {
+      const response = await fetch('/components/history/read', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -30,17 +31,38 @@ const History = () => {
       }
 
       const urls = await response.json()
-        setURLs(urls.data)
+      setURLs(urls.data)
     };
     fetchHistory();
   }, []);
 
+  const handleDownload = async (url: string) => {
+    const res = await fetch(url);
+    const image = await res.blob();
+    const blobUrl = URL.createObjectURL(image);
+
+    const anchor = document.createElement('a');
+    anchor.style.display = 'none';
+
+    anchor.href = blobUrl;
+    anchor.download = 'image.png';
+
+    document.body.appendChild(anchor);
+    anchor.click();
+  }
+
   return(
-    <main className="flex h-fit flex-row flex-wrap justify-ceneter items-center">
-      <div className='flex flex-wrap flex-row space-x-5 space-y-5'>
+    <main className="flex h-fit flex-row flex-wrap justify-center items-center">
+      <div className='flex flex-wrap flex-row space-x-10 space-y-5'>
         {
+          URLs &&
           URLs.map((url: any, index) => (
-            <img src={url.signedUrl} key={index} width={256} height={256} className='rounded'/>
+            <div className='relative group'>
+              <Image src={url.signedUrl} key={index} width={256} height={256} className='rounded' alt={url.name} />
+              <div className='absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500 rounded'>
+                <button onClick={() => handleDownload(url.signedUrl)} className='text-white'>Download</button>
+              </div>
+            </div>
           ))
         }
       </div>
